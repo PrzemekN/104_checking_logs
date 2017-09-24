3d13afe054d5e0b84cd6ed0ace9acc63c00ac114
PrzemekN <kompas.raczyn@gmail.com>
2017-09-24 20:49:42 +0200

    Update 104_checking_logs.py

--- 104_checking_logs.py
+++ 104_checking_logs.py
@@ -50,8 +50,8 @@ startlocaltimeformatted=startlocaltime.replace(':','')
 startlocaltimeformatted=startlocaltimeformatted.replace(' ','')
 nazwaPliku='c:\\testy\\summary'+startlocaltimeformatted+'.log'
 podsumowanie=open(nazwaPliku, 'w')
-podsumowanie.write(sys.argv[0]+'\n')
-podsumowanie.write(sys.argv[1]+'\n')
+podsumowanie.write(sys.argv[0]+"\n")
+podsumowanie.write(sys.argv[1]+"\n")
 
 #przejscie po podkatalogach w katalogu okreslonym jako wywolanie arg[2]
 for k in range(0,quantity_of_folders):
